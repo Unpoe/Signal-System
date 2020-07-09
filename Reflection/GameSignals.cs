a3829d5e553b0768192a8b1896043b8718264836
Unpoe <35268535+Unpoe@users.noreply.github.com>
2020-07-09 16:59:49 +0200

    Update GameSignals.cs

--- Reflection/GameSignals.cs
+++ Reflection/GameSignals.cs
@@ -3,12 +3,13 @@ using Signals;
 
 public static class GameSignals
 {
-    public static readonly Signal DayFinished = new Signal("Day Finished");
-    public static readonly Signal PlayerDamaged = new Signal("Player Damaged");
+    //In this implementation, I use a static class to manage all the instances of the signals
+    //public static readonly Signal DayFinished = new Signal("Day Finished");
+    //public static readonly Signal PlayerDamaged = new Signal("Player Damaged");
 
     private static Dictionary<string, Signal> SIGNALS = new Dictionary<string, Signal>() {
-            {DayFinished.Name, DayFinished },
-            {PlayerDamaged.Name, PlayerDamaged }
+    //        {DayFinished.Name, DayFinished },
+    //        {PlayerDamaged.Name, PlayerDamaged }
         };
 
     public static Signal GetSignalByName(string signalName) {
